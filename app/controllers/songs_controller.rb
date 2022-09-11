2a5c0baaea13f1e24912fcb47b479a09ce1eae05
Michael <mtrix92@gmail.com>
2022-09-11 13:36:35 -0400

    commit tree 18d4f8754e78a759e0f9e58a4a3cc8356764fa6b

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,36 +1,7 @@
 require 'rack-flash'
-class ApplicationController < Sinatra::Base
-  register Sinatra::ActiveRecordExtension
-
-  enable :sessions
-  set :session_secret, "my_application_secret"
-  set :views, Proc.new { File.join(root, "../views/") }
-  use Rack::Flash
-
-  get '/' do
-    erb :index
-  end
-
-  get '/artists' do
-    @artists = Artist.all
-    erb:'artists/index'
-  end
-
-  get '/artists/:slug' do
-    @artist = Artist.find_by_slug(params[:slug])
-    erb :'artists/show'
-  end
-
-  get '/genres' do
-    @genres = Genre.all
-    erb :'genres/index'
-  end
-
-  get '/genres/:slug' do
-    @genre = Genre.find_by_slug(params[:slug])
-    erb :'/genres/show'
-  end
-
+class SongsController < ApplicationController
+    use Rack::Flash
+  
     get '/songs' do
       @songs = Song.all
       erb :'/songs/index'
@@ -72,5 +43,4 @@ class ApplicationController < Sinatra::Base
       redirect("/songs/#{@song.slug}")
     end
   
-
-end
+  end
